a6870a681eb2adda362de09d667c11469c09d777
widadanncoud <71724347+widadanncoud@users.noreply.github.com>
2021-10-12 17:37:18 +0200

    challenge2

--- Sessie 2/MiniGame/src/main/kotlin/main.kt
+++ Sessie 2/MiniGame/src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 fun main() {
     println("You will be playing some games about Rick and Morty.")
     challengeOne()
+    challengeTwo()
 }
 
 
@@ -19,3 +20,48 @@ fun challengeOne(){
         println("you lose")
     }
 }
+
+fun challengeTwo(){
+
+    println("Guess the correct number.")
+
+    val diceResult = setOf<Int>(1, 2, 3, 4, 5, 6).random()
+
+    val userResult = readLine()
+        if (userResult == "low"){
+            if (arrayOf(1,2,3).contains(diceResult)){
+                println("winner")
+            }else{
+                println("loser")
+            }
+        }else{
+            if (arrayOf(4,5,6).contains(diceResult)){
+                println("winner")
+            }else{
+                println("loser")
+            }
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
